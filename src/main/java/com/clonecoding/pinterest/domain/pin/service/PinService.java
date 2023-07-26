de15101c34b8defdd865b46a619cf0ca323c341c
MyungHyun Lee <dvkk12@gmail.com>
2023-07-26 16:03:49 +0900

    feat : "Apache Tika 추가 파일 식별 후 이미지 파일만 전송"

--- src/main/java/com/clonecoding/pinterest/domain/pin/service/PinService.java
+++ src/main/java/com/clonecoding/pinterest/domain/pin/service/PinService.java
@@ -5,15 +5,21 @@ import com.clonecoding.pinterest.domain.pin.dto.PinRequestDTO;
 import com.clonecoding.pinterest.domain.pin.dto.PinResponseDTO;
 import com.clonecoding.pinterest.domain.pin.entity.Pin;
 import com.clonecoding.pinterest.domain.pin.repository.PinRepository;
+import com.clonecoding.pinterest.domain.pin.util.imgUtil;
 import com.clonecoding.pinterest.domain.user.entity.User;
 import com.clonecoding.pinterest.domain.user.entity.UserRoleEnum;
 import com.clonecoding.pinterest.domain.user.repository.UserRepository;
 import com.clonecoding.pinterest.global.S3.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PinService {
@@ -25,19 +31,28 @@ public class PinService {
 
 
     //생성
-    public PinResponseDTO createPin(Long id, PinRequestDTO requestDTO){
+    public PinResponseDTO createPin(Long id, PinRequestDTO requestDTO) {
         User user = userRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("찾을 수 없습니다.")
         );
-        String imgFileUrl = null;
 
+
+        String imgFileUrl = null;
         MultipartFile imageFile = requestDTO.getImageFile();
-        if (!imageFile.isEmpty()){
-            try{
+
+        try(InputStream inputStream = imageFile.getInputStream()){
+            log.info("image file 변환." + imageFile.getContentType());
+
+            if(!imageFile.isEmpty()){
+                boolean isValid = imgUtil.validImgFile(inputStream);
+                if(!isValid){
+                    throw new IllegalArgumentException("이미지 파일만 올려주세요.");
+                }
                 imgFileUrl = s3Service.uploadFile(imageFile);
-            }catch(Exception e){
-                throw e;
             }
+
+        }catch(IOException e){
+            e.printStackTrace();
         }
 
         Pin pin = new Pin(imgFileUrl, user);
@@ -66,12 +81,20 @@ public class PinService {
         String imgFileUrl = null;
 
         MultipartFile imageFile = requestDTO.getImageFile();
-        if(!imageFile.isEmpty()){
-            try{
+
+        try(InputStream inputStream = imageFile.getInputStream()){
+            log.info("image file 변환." + imageFile.getContentType());
+
+            if(!imageFile.isEmpty()){
+                boolean isValid = imgUtil.validImgFile(inputStream);
+                if(!isValid){
+                    throw new IllegalArgumentException("이미지 파일만 올려주세요.");
+                }
                 imgFileUrl = s3Service.uploadFile(imageFile);
-            }catch (Exception e){
-                throw e;
             }
+
+        }catch(IOException e){
+            e.printStackTrace();
         }
 
         if(hasRole(user,pin)){
