97fa156b078777d56c65500c61582ac89ca26849
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-07-26 13:51:30 +0900

    init IdenticonService

--- src/main/java/com/clonecoding/pinterest/domain/user/entity/User.java
+++ src/main/java/com/clonecoding/pinterest/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import com.clonecoding.pinterest.domain.user.dto.UserCreateRequestDto;
 import com.clonecoding.pinterest.global.entity.TimeStamped;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,7 +24,7 @@ public class User extends TimeStamped {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String username;
 
     @Column(nullable = false)
@@ -35,6 +36,16 @@ public class User extends TimeStamped {
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
+
+    @Builder
+    public User (String email, String username, String password, UserRoleEnum role,String profileImageUrl){
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.profileImageUrl =  profileImageUrl;
+    }
+
 
     public User(UserCreateRequestDto requestDto){
         this.email = requestDto.getEmail();
